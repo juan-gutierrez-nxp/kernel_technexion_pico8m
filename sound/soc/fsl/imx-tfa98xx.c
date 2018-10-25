fd7224b6f84db2d5703f8ef3fbbf0748eb19634d
Adrian Alonso <adrian.alonso@nxp.com>
2018-10-24 19:25:52 -0500

    sound: soc: fsl: imx tfa98xx: multi codec conf
    Rework use codec_conf for multiple codec instances
    
    Signed-off-by: Adrian Alonso <adrian.alonso@nxp.com>

--- sound/soc/fsl/imx-tfa98xx.c
+++ sound/soc/fsl/imx-tfa98xx.c
@@ -43,6 +43,8 @@ static struct snd_pcm_hw_constraint_list imx_tfa98xx_rate_constraints = {
 struct snd_soc_card_drvdata_imx_tfa {
 	struct snd_soc_dai_link *dai;
 	struct snd_soc_card card;
+	struct snd_soc_codec_conf *codec_conf;
+	int num_codec_conf;
 	char codec_dai_name[DAI_NAME_SIZE];
 	char platform_name[DAI_NAME_SIZE];
 	int pstreams;
@@ -109,6 +111,7 @@ static int imx_tfa98xx_hw_params(struct snd_pcm_substream *substream,
 		dev_err(cpu_dai->dev, "failed to set cpu sysclk: %d\n", ret);
 		return ret;
 	}
+
 	return ret;
 }
 
@@ -196,16 +199,23 @@ static void *tfa_devm_kstrdup(struct device *dev, char *buf)
  * The structs are used as place holders. They will be completely
  * filled with data from dt node.
  */
-	
-static struct snd_soc_dai_link imx_dai_tfa98xx[] = {
+
+static struct snd_soc_dai_link_component tfa98xx_codecs[] = {
 	{
-		.name		= "HiFi",
-		.stream_name	= "HiFi",
-		.ops            = &imx_tfa98xx_ops,
-		.init           = imx_tfa98xx_init,
-		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_CBS_CFS |
-			SND_SOC_DAIFMT_NB_NF,
+		.dai_name = "tfa98xx-aif",
 	},
+	{
+		.dai_name = "tfa98xx-aif",
+	}
+};
+
+static struct snd_soc_dai_link imx_dai_tfa98xx = {
+	.name = "tfa98xx",
+	.stream_name = "Audio",
+	.codecs = tfa98xx_codecs,
+	.num_codecs = 2,
+	.ignore_pmdown_time = 1,
+	.ops = &imx_tfa98xx_ops,
 };
 
 static const struct of_device_id imx_tfa98_dt_ids[] = {
@@ -228,8 +238,6 @@ static const struct snd_kcontrol_new imx_tfa98xx_controls[] = {
 static struct snd_soc_card imx_tfa98xx_soc_card = {
 	.owner = THIS_MODULE,
 	.name = "TFA9912",	/* default name if none defined in DT */
-	.dai_link = imx_dai_tfa98xx,
-	.num_links = ARRAY_SIZE(imx_dai_tfa98xx),
 	.controls = imx_tfa98xx_controls,
 	.num_controls = ARRAY_SIZE(imx_tfa98xx_controls),
 	.dapm_widgets = imx_tfa98xx_widgets,
@@ -239,10 +247,10 @@ static struct snd_soc_card imx_tfa98xx_soc_card = {
 static int imx_tfa98xx_probe(struct platform_device *pdev)
 {
 	struct device_node *cpu_np, *np = pdev->dev.of_node;
+	struct device_node *codec_np_0, *codec_np_1 = NULL;
 	struct platform_device *cpu_pdev;
 	struct snd_soc_dai_link_component *codecs;
 	struct i2c_client *codec_dev;
-	struct snd_soc_dai_link *dai;
 	struct snd_soc_card_drvdata_imx_tfa *drvdata = NULL;
 	int ret = 0;
 	int i, num_codecs;
@@ -251,7 +259,13 @@ static int imx_tfa98xx_probe(struct platform_device *pdev)
 
 	imx_tfa98xx_soc_card.dev = &pdev->dev;
 
-	cpu_np = of_parse_phandle(pdev->dev.of_node, "cpu-dai", 0);
+	drvdata = devm_kzalloc(&pdev->dev, sizeof(*drvdata), GFP_KERNEL);
+	if (!drvdata) {
+		ret = -ENOMEM;
+		goto fail;
+	}
+
+	cpu_np = of_parse_phandle(pdev->dev.of_node, "audio-cpu", 0);
 	if (!cpu_np) {
 		dev_err(&pdev->dev, "cpu dai phandle missing or invalid\n");
 		ret = -EINVAL;
@@ -264,69 +278,72 @@ static int imx_tfa98xx_probe(struct platform_device *pdev)
 		ret = -EINVAL;
 		goto fail;
 	}
+
+	codec_np_0 = of_parse_phandle(np, "audio-codec", 0);
+	if (!codec_np_0) {
+		dev_err(&pdev->dev, "audio codec phandle missing or invalid\n");
+		ret = -EINVAL;
+		goto fail;
+	}
+
+	codec_dev = of_find_i2c_device_by_node(codec_np_0);
+	if (!codec_dev || !codec_dev->dev.driver) {
+		dev_err(&pdev->dev, "fail to find codec 0 platform device\n");
+		ret = -EINVAL;
+		goto fail;
+	}
+
+	codec_np_1 = of_parse_phandle(np, "audio-codec", 1);
+	if (!codec_np_1) {
+		dev_err(&pdev->dev, "audio codec phandle missing or invalid\n");
+		ret = -EINVAL;
+		goto fail;
+	}
+
+	codec_dev = of_find_i2c_device_by_node(codec_np_1);
+	if (!codec_dev || !codec_dev->dev.driver) {
+		dev_err(&pdev->dev, "fail to find codec 1 platform device\n");
+		ret = -EINVAL;
+		goto fail;
+	}
+
+	drvdata->num_codec_conf = 2;
+	drvdata->codec_conf = devm_kzalloc(&pdev->dev,
+		drvdata->num_codec_conf * sizeof(struct snd_soc_codec_conf),
+		GFP_KERNEL);
+
+	if (!drvdata->codec_conf) {
+		ret = -ENOMEM;
+		goto fail;
+	}
+
 	ret = snd_soc_of_parse_card_name(&imx_tfa98xx_soc_card, "nxp,model");
 	if (ret)
-		goto fail;;
+		goto fail;
 
 	ret = snd_soc_of_parse_audio_routing(&imx_tfa98xx_soc_card, "nxp,audio-routing");
 	if (ret)
 		goto fail;
 
-	num_codecs = of_count_phandle_with_args(np, "nxp,audio-codec", NULL);
-	if (num_codecs < 1) {
-		ret = -EINVAL;
-		goto fail;
-	}
-	pr_info("Found %d codec(s)\n", num_codecs);
+	drvdata->codec_conf[0].name_prefix = "0";
+	drvdata->codec_conf[0].of_node = codec_np_0;
+	drvdata->codec_conf[1].name_prefix = "1";
+	drvdata->codec_conf[2].of_node = codec_np_1;
 
-	codecs = devm_kzalloc(&pdev->dev,
-			sizeof(struct snd_soc_dai_link_component) * num_codecs,
-			GFP_KERNEL);
+	tfa98xx_codecs[0].of_node = codec_np_0;
+	tfa98xx_codecs[1].of_node = codec_np_1;
+	/* TODO: reimplememt */
+	tfa98xx_codecs[0].dai_name = "tfa98xx-aif-1-34";
+	tfa98xx_codecs[1].dai_name = "tfa98xx-aif-1-35";
 
-	if (!codecs) {
-		ret = -ENOMEM;
-		goto fail;
-	}
+	imx_dai_tfa98xx.cpu_dai_name = dev_name(&cpu_pdev->dev);
+	imx_dai_tfa98xx.platform_of_node = cpu_np;
 
-	for (i = 0; i < num_codecs; i++) {
-		struct device_node *parent;
-		int id;
-		const u32 *property;
-		int len;
-		char name[18];
-
-		codecs[i].of_node = of_parse_phandle(np, "nxp,audio-codec", i);
-		/* get i2c address */
-		property = of_get_property(codecs[i].of_node, "reg", &len);
-		/* get i2c bus */
-		parent = of_get_parent(codecs[i].of_node);
-		id = of_alias_get_id(parent, "i2c");
-		/* construct dai name */
-		snprintf(name, sizeof(name), "tfa98xx-aif-%d-%x", id, be32_to_cpu(property[0]));
-		codecs[i].dai_name = tfa_devm_kstrdup(&pdev->dev, name);
-	}
-	dai = &imx_dai_tfa98xx[0];
-	dai->platform_of_node = cpu_np;
-	dai->codecs = codecs;
-	dai->cpu_dai_name = dev_name(&cpu_pdev->dev);
-	dai->num_codecs = num_codecs;
-	dai->cpu_of_node = of_parse_phandle(np, "ssi-controller", 0);
-	if (!dai->cpu_of_node) {
-		ret = -EINVAL;
-		goto fail;
-	}
-
-	/* Only set the platform_of_node if the platform_name is not set */
-	if (!dai->platform_name)
-		dai->platform_of_node = dai->cpu_of_node;
-
-	drvdata = devm_kzalloc(&pdev->dev, sizeof(*drvdata), GFP_KERNEL);
-	if (!drvdata) {
-		ret = -ENOMEM;
-		goto fail;
-	}
-
-	drvdata->dai = dai;
+	imx_tfa98xx_soc_card.num_links = 1;
+	imx_tfa98xx_soc_card.dai_link = &imx_dai_tfa98xx;
+	imx_tfa98xx_soc_card.owner = THIS_MODULE;
+	imx_tfa98xx_soc_card.codec_conf = drvdata->codec_conf;
+	imx_tfa98xx_soc_card.num_configs = drvdata->num_codec_conf;
 
 	platform_set_drvdata(pdev, &drvdata->card);
 	snd_soc_card_set_drvdata(&imx_tfa98xx_soc_card, drvdata);
@@ -340,10 +357,10 @@ static int imx_tfa98xx_probe(struct platform_device *pdev)
 fail:
 	if (cpu_np)
 		of_node_put(cpu_np);
-#if 0
-	if (codecs)
-		of_node_put(codecs);
-#endif
+	if (codec_np_0)
+		of_node_put(codec_np_0);
+	if (codec_np_1)
+		of_node_put(codec_np_1);
 
 	return ret;
 }
